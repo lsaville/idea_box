ec2a42eb1470b5b4137c310e2c6a5cf4dc07c726
Lee Saville <l.w.saville@gmail.com>
2016-10-25 15:44:13 -0600

    Create category model spec

--- spec/feature/users/user_can_sign_up_spec.rb
+++ spec/feature/users/user_can_sign_up_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-describe "User can sign up" do
-  scenario "they visit the sign up page and create an account" do
-    visit new_user_path
-
-    fill_in "user_name", with: "Fran"
-    fill_in "email", with: "f@gmail.com"
-    fill_in "password", with: "boom"
-    click_on "Create account"
-
-    expect(page).to have_content "Hi, Fran"
-  end
-end
+# describe "User can sign up" do
+#   scenario "they visit the sign up page and create an account" do
+#     visit new_user_path
+#
+#     fill_in "user_name", with: "Fran"
+#     fill_in "email", with: "f@gmail.com"
+#     fill_in "password", with: "boom"
+#     click_on "Create account"
+#
+#     expect(page).to have_content "Hi, Fran"
+#   end
+# end
---
+++ spec/model/category_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe Category do
+  describe "validations" do
+    context "valid attributes" do
+    end
+
+    context "invalid attributes" do
+    end
+  end
+
+  describe "Relationships" do
+    it "has many ideas" do
+  
+    end
+  end
+end
