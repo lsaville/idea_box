64311e611f38bcebb7cfa2ebc1d5d1ddcaf42409
Lee Saville <l.w.saville@gmail.com>
2016-10-26 14:45:07 -0600

    Create create and show and get user can sign up spec passign

--- spec/features/users/user_can_sign_up_spec.rb
+++ spec/features/users/user_can_sign_up_spec.rb
@@ -4,10 +4,10 @@ describe "User can sign up" do
   scenario "they visit the sign up page and create an account" do
     visit new_user_path
 
-    fill_in "name", with: "Fran"
-    fill_in "email", with: "f@gmail.com"
-    fill_in "password", with: "boom"
-    fill_in "password_confirmation", with: "boom"
+    fill_in "Name", with: "Fran"
+    fill_in "Email", with: "f@gmail.com"
+    fill_in "Password", with: "boom"
+    fill_in "Password confirmation", with: "boom"
     click_on "Create account"
 
     expect(page).to have_content "Hi, Fran"
