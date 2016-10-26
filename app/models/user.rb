9b686e5f08d799bad5b82b03fec802012ffbafe7
Lee Saville <l.w.saville@gmail.com>
2016-10-26 15:13:46 -0600

    White space change

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   validates :name, :email, :password, presence: true
   validates :email, uniqueness: true
   validates_confirmation_of :password
+
   has_many :ideas
+
   has_secure_password
 end
