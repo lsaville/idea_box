57b77012202051938232d79d7ad5471ab7363fac
Lee Saville <l.w.saville@gmail.com>
2016-10-26 19:56:33 -0600

    Create new create and index methods to pass feature test can-create-category

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,25 @@
+class CategoriesController < ApplicationController
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "Successfully created category!"
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @categories = Category.all
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
